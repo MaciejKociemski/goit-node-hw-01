53c29efaad1a393ca77f2a40e008a03d1e10d54f
Maciej Kociemski <maciejkociemski@MacBook-Pro-Maciej.local>
2023-07-24 10:39:48 +0200

    fix bug in index.js (getContactById on getContactsById..)

--- contacts.js
+++ contacts.js
@@ -1,44 +1,41 @@
-const fs = require("fs").promises
-require("colors");
-const path = require("path")
+const fs = require("fs");
+
+const path = require("path");
 
 const contactsPath = path.join(__dirname, "db", "contacts.json");
 
 function listContacts() {
-    const data = fs.readFileSync(contactsPath, 'utf-8');
-    return JSON.parse(data);
-
+  const data = fs.readFileSync(contactsPath, "utf-8");
+  return JSON.parse(data);
 }
 
 function getContactsById(contactId) {
-    const contacts = listContacts();
-    return contacts.find(contact => contact.id === contactId);
-
+  const contacts = listContacts();
+  return contacts.find((contact) => contact.id === contactId);
 }
 
 function removeContact(contactId) {
-    const contacts = listContacts();
-    const updateContacts = contact.filter(contact => contact.id !== contactId);
-    fs.writeFileSync(contactsPath, JSON.stringify(updatedContacts, null, 2));
+  const contacts = listContacts();
+  const updateContacts = contacts.filter((contact) => contact.id !== contactId);
+  fs.writeFileSync(contactsPath, JSON.stringify(updatedContacts, null, 2));
 }
 
 function addContact(name, email, phone) {
-    
-    const contacts = listContacts();
-    const newContact = {
-        id: Date.now().toString(),
-        name,
-        email,
-        phone
-    };
-    contacts.push(newContact);
-    fs.writeFileSync(contactsPath, JSON.stringify(contacts, null, 2));
-    return newContact;
+  const contacts = listContacts();
+  const newContact = {
+    id: Date.now().toString(),
+    name,
+    email,
+    phone,
+  };
+  contacts.push(newContact);
+  fs.writeFileSync(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
 }
 
 module.exports = {
-    listContacts,
-    getContactsById,
-    removeContact,
-    addContact
-}
+  listContacts,
+  getContactsById,
+  removeContact,
+  addContact,
+};
--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-const contacts = require('./contacts');
+const contacts = require("./contacts");
+
+const argv = require("yargs").argv;
+
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      console.table(contacts.listContacts());
+      break;
+
+    case "get":
+      console.log(contacts.getContactsById(id));
+      break;
+
+    case "add":
+      console.log(contacts.addContact(name, email, phone));
+      break;
+
+    case "remove":
+      contacts.removeContact(id);
+      console.log(`Contact with id ${id} has been removed.`);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
