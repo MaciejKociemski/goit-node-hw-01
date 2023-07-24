b495bb7849d24283ff9a7e9f5c88208d788da699
Maciej Kociemski <maciejkociemski@MacBook-Pro-Maciej.local>
2023-07-24 10:15:28 +0200

    feat: add function:listContacts,getContactsById, removeContact,addContact

---
+++ contacts.js
@@ -0,0 +1,44 @@
+const fs = require("fs").promises
+require("colors");
+const path = require("path")
+
+const contactsPath = path.join(__dirname, "db", "contacts.json");
+
+function listContacts() {
+    const data = fs.readFileSync(contactsPath, 'utf-8');
+    return JSON.parse(data);
+
+}
+
+function getContactsById(contactId) {
+    const contacts = listContacts();
+    return contacts.find(contact => contact.id === contactId);
+
+}
+
+function removeContact(contactId) {
+    const contacts = listContacts();
+    const updateContacts = contact.filter(contact => contact.id !== contactId);
+    fs.writeFileSync(contactsPath, JSON.stringify(updatedContacts, null, 2));
+}
+
+function addContact(name, email, phone) {
+    
+    const contacts = listContacts();
+    const newContact = {
+        id: Date.now().toString(),
+        name,
+        email,
+        phone
+    };
+    contacts.push(newContact);
+    fs.writeFileSync(contactsPath, JSON.stringify(contacts, null, 2));
+    return newContact;
+}
+
+module.exports = {
+    listContacts,
+    getContactsById,
+    removeContact,
+    addContact
+}
